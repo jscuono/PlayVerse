6236d4d2c296088a9cfe5b13ef8058e01a1dd959
jscuono <jscuono@icloud.com>
2026-07-20 18:01:13 -0400

    Add files via upload

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'pages/auth_page.dart';
 
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorObservers: [routeObserver],
       home: AuthPage(),
     );
   }
